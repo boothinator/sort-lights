3bd125a46c3a403db78b573c6a71d145d5e61715
boothinator <boothinator@users.noreply.github.com>
2019-03-05 11:13:05 -0500

    Update sort-lights.ino
    Add single pixel move and make lots of fixes

--- sort-lights.ino
+++ sort-lights.ino
@@ -4,14 +4,15 @@
  * Light up different pixel based on the the selection when in select algorithm mode.
  * Press third button run? Wait a few seconds? Press both?
  * Use RoboRIO input to select?
+ * use EEPROM to store settings
  */
 
 #include <Adafruit_NeoPixel.h>
 #include <limits.h>
 
-#define ARRAY_SIZE 144
+#define ARRAY_SIZE 10
 
-#define PIN 6
+#define PIN 7
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -24,8 +25,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(ARRAY_SIZE, PIN, NEO_GRB + NEO_KHZ80
 
 void randomize() {
   for (int i = 0; i < ARRAY_SIZE; i++) {
-    strip.setPixelColor(i, random(UINT_MAX));
-  }  
+    strip.setPixelColor(i, random(0xFFFFFF));
+  }
 }
 
 void setup() {
@@ -38,27 +39,61 @@ void setup() {
   randomSeed(analogRead(0));
   
   strip.begin();
+  strip.show();
+}
+
+void blink(int i, int j)
+{
+  uint32_t color_i = strip.getPixelColor(i);
+  uint32_t color_j = strip.getPixelColor(j);
+  for (int k = 0; k < 5; k++) {
+    strip.setPixelColor(i, 0xFFFFFF);
+    strip.setPixelColor(j, 0xFFFFFF);
+    strip.show();
+    delay(100);
+    strip.setPixelColor(i, color_i);
+    strip.setPixelColor(j, color_j);
+    strip.show();
+    delay(100);
+  }
 }
 
 void insertionSort()
 {
   randomize();
-  strip.display();
+  strip.show();
   
   for (int i = 1; i < ARRAY_SIZE; i++) {
     for (int j = i; j >= 0; j--){
       if (strip.getPixelColor(j - 1) > strip.getPixelColor(j)) {
+        blink(j-1, j);
         uint32_t tmp = strip.getPixelColor(j - 1);
         strip.setPixelColor(j - 1, strip.getPixelColor(j));
         strip.setPixelColor(j, tmp);
-        strip.display();
+        strip.show();
+        delay(500);
       }
     }
   }
 }
 
-void loop() {
-  while (true) {
-    insertionSort();
+void singlePixel(uint32_t color)
+{
+  Serial.print(color, HEX);
+  strip.setPixelColor(0, color);
+  strip.show();
+  delay(500);
+  for (int i = 1; i < ARRAY_SIZE; i++) {
+    strip.setPixelColor(i - 1, 0);
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(500);
   }
+  strip.setPixelColor(ARRAY_SIZE - 1, 0);
+  strip.show();
+}
+
+void loop() {
+  //insertionSort();
+  singlePixel(random(0xFFFFFF));
 }
