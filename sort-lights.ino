513bc0b05725f7f0cb19207984245e9ffa9ace07
boothinator <boothinator@users.noreply.github.com>
2019-03-23 20:04:53 -0400

    Update sort-lights.ino
    Lots of changes

--- sort-lights.ino
+++ sort-lights.ino
@@ -8,30 +8,29 @@
  */
 #include "FastLED.h"
 
-#define ARRAY_SIZE 10
+#define ARRAY_SIZE 25
 CRGB leds[ARRAY_SIZE];
 #define PIN 7
 
 #define SORT_BRIGHTNESS 0x10
+#define SINGLE_PIXEL_BRIGHTNESS 0x10
+#define RAINBOW_BRIGHTNESS 0x10
+
+uint8_t getRandomHue(int numberOfColors)
+{
+  return (random(numberOfColors)/(double)numberOfColors)*0xFF;
+}
+
+CRGB getRandomColor(int numberOfColors, int saturation, int value)
+{
+  return CHSV(getRandomHue(numberOfColors), saturation, value);
+}
 
 void randomize() {
   for (int i = 0; i < ARRAY_SIZE; i++) {
-    uint8_t hue = (random(ARRAY_SIZE)/(double)ARRAY_SIZE)*0xFF;
+    uint8_t hue = getRandomHue(ARRAY_SIZE);
     leds[i].setHSV(hue, 255, SORT_BRIGHTNESS);
   }
-}
-
-void setup() {
-  Serial.begin(9600);
-
-  // if analog input pin 0 is unconnected, random analog
-  // noise will cause the call to randomSeed() to generate
-  // different seed numbers each time the sketch runs.
-  // randomSeed() will then shuffle the random function.
-  randomSeed(analogRead(0));
-  
-  FastLED.addLeds<NEOPIXEL, PIN>(leds, ARRAY_SIZE);
-  FastLED.show();
 }
 
 void blink(int i, int j, int times)
@@ -70,26 +69,57 @@ void insertionSort()
     }
     delay(1000);
   }
+  delay(5000);
 }
 
-void singlePixel(CRGB color)
+void shift()
 {
-  Serial.print(color, HEX);
-  leds[0] = color;
-  FastLED.show();
-  delay(500);
-  for (int i = 1; i < ARRAY_SIZE; i++) {
-    leds[i - 1] = 0;
-    leds[i] = color;
-    FastLED.show();
-    delay(500);
+  CRGB tmpColor = leds[ARRAY_SIZE - 1];
+  for (int i = ARRAY_SIZE - 1; i > 0; i--) {
+    leds[i] = leds[i-1];
   }
-  leds[ARRAY_SIZE - 1] = 0;
+  leds[0] = tmpColor;
   FastLED.show();
+}
+
+void rainbow(int rainbowPixels)
+{
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    leds[i].setHSV((i%rainbowPixels/(double)rainbowPixels)*0xFF, 0xFF, RAINBOW_BRIGHTNESS);
+    FastLED.show();
+  }
+}
+
+void marquee(CRGB color, int onPixels, int offPixels)
+{
+  for (int i = 0; i < ARRAY_SIZE; i += onPixels + offPixels) {
+    for (int j = 0; j < onPixels; j++)
+    {
+      leds[i+j] = color;
+    }
+  }
+  FastLED.show();
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+  
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, ARRAY_SIZE);
+  FastLED.show();
+
+  //rainbow(4);
+  //leds[0] = getRandomColor(16, 255, SINGLE_PIXEL_BRIGHTNESS);
+  marquee(CHSV(0, 0x00, SINGLE_PIXEL_BRIGHTNESS), 2, 1);
 }
 
 void loop() {
-  insertionSort();
-  delay(5000);
-  //singlePixel(CHSV(random(0xFF), 255, 255));
+  //insertionSort();
+  shift();
+  delay(500);
 }
